b4d5ecf61a966f392bb2a6e7dca39f2d5aac88bb
azukisiba <azukisiba398@gmail.com>
2021-10-11 11:06:59 +0900

    ユーザー単体テストコードの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname { '太郎' }
+    email { Faker::Internet.free_email }
+    password { 'aaa000' }
+    password_confirmation { password }
   end
 end
